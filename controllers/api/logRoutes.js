1045e00758cc326186e17be4626fe3b8bbc5e28a
Sabrina Hanson <sabhanson7@gmail.com>
2022-09-11 13:14:11 -0700

    view for posting a new log. post route works and saves to database

--- controllers/api/logRoutes.js
+++ controllers/api/logRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Log = require("../../models/Log");
 
-// GET all logs
+// GET all logs '/api/logs'
 router.get("/", async (req, res) => {
   try {
     const logData = await Log.findAll();
@@ -9,19 +9,32 @@ router.get("/", async (req, res) => {
     const logs = logData.map((log) => log.get({ plain: true }));
 
     res.render("dashboard", {
+      layout: "main",
       logs,
     });
-    res.status(200).json(logs);
   } catch (err) {
     res.status(400).json(err);
   }
 });
-// CREATE a log
-router.post("/", async (req, res) => {
+
+// GET the "create new log" form
+router.get("/newLog", (req, res) => {
+  try {
+    res.render("newLog", {
+      layout: "main",
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// CREATE/POST a new log '/api/logs/newLog'
+router.post("/newLog", async (req, res) => {
+  const body = req.body;
   try {
     const newLog = await Log.create({
-      workout_type: req.body.workout_type,
-      details: req.body.details,
+      workout_type: body.workout_type,
+      details: body.details,
       // workout_type and details
     });
 
---
+++ public/js/newLog.js
@@ -0,0 +1,26 @@
+const newFormHandler = async function (event) {
+  event.preventDefault();
+  console.log("hello");
+
+  const workoutType = document.querySelector(
+    'input[name="workout-type"]'
+  ).value;
+  const workoutDetails = document.querySelector(
+    'textarea[name="workout-details"]'
+  ).value;
+
+  await fetch(`/api/logs/newLog`, {
+    method: "POST",
+    body: JSON.stringify({
+      workout_type: workoutType,
+      details: workoutDetails,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  document.location.replace("/api/logs/newLog");
+};
+
+document
+  .querySelector("#new-log-form")
+  .addEventListener("click", newFormHandler);
