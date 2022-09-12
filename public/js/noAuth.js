e067dc2ae7b9423c9e1f4b11815e77c8c56ccbe7
Sabrina Hanson <sabhanson7@gmail.com>
2022-09-12 16:37:01 -0700

    added button to toggle between signup and login pages

--- public/js/noAuth.js
+++ public/js/noAuth.js
@@ -2,4 +2,9 @@ function loginRedirect() {
   document.location.replace("/login");
 }
 
+function signupRedirect() {
+  document.location.replace("/signup");
+}
+
 document.querySelector("#login").addEventListener("click", loginRedirect);
+document.querySelector("#signup").addEventListener("click", signupRedirect);
