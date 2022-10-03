bc2d6dd3e4f0f19be751d89ba55510de00bd6e93
Sabrina Hanson <sabhanson7@gmail.com>
2022-10-02 19:41:09 -0700

    views/noAuth.js MVP

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ an easy to use workout tracker
 - [chart JS](https://www.chartjs.org/docs/latest/)
 - [favicon](https://www.favicon.cc/?action=edit_image&file_id=936438)
 - [reference for remember me checkbox](https://codepen.io/AllThingsSmitty/pen/pOoeyz)
+- [running image by Orest Sv: ](https://www.pexels.com/photo/photo-of-person-running-on-dirt-road-1821694/)
+- [biking image by Nataliya Vaitkevich](https://www.pexels.com/photo/people-biking-on-a-dirt-road-5712935/])
--- public/js/noAuth.js
+++ public/js/noAuth.js
@@ -1,10 +1,10 @@
-function loginRedirect() {
-  document.location.replace("/login");
-}
+// function loginRedirect() {
+//   document.location.replace("/login");
+// }
 
-function signupRedirect() {
-  document.location.replace("/signup");
-}
+// function signupRedirect() {
+//   document.location.replace("/signup");
+// }
 
-document.querySelector("#login").addEventListener("click", loginRedirect);
-document.querySelector("#signup").addEventListener("click", signupRedirect);
+// document.querySelector("#login").addEventListener("click", loginRedirect);
+// document.querySelector("#signup").addEventListener("click", signupRedirect);
