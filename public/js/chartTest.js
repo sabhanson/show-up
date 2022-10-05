e373e17f26109ac466aade6751eef70223c5d5d0
Sabrina Hanson <sabhanson7@gmail.com>
2022-10-04 19:22:47 -0700

    added chart element to dashboard

--- public/js/chartTest.js
+++ public/js/chartTest.js
@@ -1,9 +1,4 @@
 const labels = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
   "June",
   "July",
   "August",
@@ -17,10 +12,27 @@ const data = {
   labels: labels,
   datasets: [
     {
-      label: "Yearly Data",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
-      data: [19, 10, 5, 2, 20, 30, 45, 30, 30, 20, 23, 12],
+      label: "",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(201, 203, 207, 0.2)",
+      ],
+      borderColor: [
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
+        "rgb(201, 203, 207)",
+      ],
+      borderWidth: 1,
     },
   ],
 };
@@ -28,6 +40,7 @@ const config = {
   type: "bar",
   data: data,
   options: {
+    color: "black",
     scales: {
       y: {
         beginAtZero: true,
